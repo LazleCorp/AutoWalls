ee7800ad84df7c05e0b4dddab59f394ef53e8eca
LaxWasHere <public@LaxWasHere.com>
2013-08-06 16:57:33 -0700

    re-wrote DeathMatch Command

--- src/com/jkush321/autowalls/commands/DeathmatchCommand.java
+++ src/com/jkush321/autowalls/commands/DeathmatchCommand.java
@@ -18,32 +18,31 @@ public class DeathmatchCommand implements CommandExecutor {
 		this.plugin = plugin;
 		// TODO Auto-generated constructor stub
 	}
-
-
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		if (sender.hasPermission("walls.op") || sender.isOp()) {
-		if (sender instanceof Player) {
-			Player p = (Player)sender;
-			Location l = p.getLocation();
-			if (args.length == 0 && plugin.gameInProgress) {
-				TeleportManager.deathMatch();
-		} else if (args.length == 1 && args[0] == "setspawn1") {
-	        	plugin.getConfig().set("SpawnX1", Double.valueOf(l.getBlockX() + 0.5));
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("You may not use SpawnTP commands, console!");
+            return false;
+        }
+        if (sender.hasPermission("autowalls.op") || sender.isOp()) {
+        Player p = (Player) sender;
+        Location l = p.getLocation();
+        if (args.length == 1) {
+        	if (args[0].equalsIgnoreCase("setspawn1")) {
+        		plugin.getConfig().set("SpawnX1", Double.valueOf(l.getBlockX() + 0.5));
 	        	plugin.getConfig().set("SpawnY1", Double.valueOf(l.getBlockY() + 0.5));
 	        	plugin.getConfig().set("SpawnZ1", Double.valueOf(l.getBlockZ() + 0.5));
 	        	plugin.saveConfig();
 	        	p.sendMessage(ChatColor.GREEN + "Deathmatch spawn for map 1 set!");
-		} else if (args.length == 1 && args[0] == "setspawn2") {
-        	plugin.getConfig().set("SpawnX2", Double.valueOf(l.getBlockX() + 0.5));
-        	plugin.getConfig().set("SpawnY2", Double.valueOf(l.getBlockY() + 0.5));
-        	plugin.getConfig().set("SpawnZ2", Double.valueOf(l.getBlockZ() + 0.5));
-        	p.sendMessage(ChatColor.GREEN + "DeathMatch spawn for map 2 set!");
-        	plugin.saveConfig();
-		} else {
-			p.sendMessage(ChatColor.RED + "Invalid stuff, ask LaxWashere");
-		} 
-	} sender.sendMessage("Nope!");
-		} return false;
+        	} if (args[0].equalsIgnoreCase("setspawn2")) {
+        		plugin.getConfig().set("SpawnX2", Double.valueOf(l.getBlockX() + 0.5));
+	        	plugin.getConfig().set("SpawnY2", Double.valueOf(l.getBlockY() + 0.5));
+	        	plugin.getConfig().set("SpawnZ2", Double.valueOf(l.getBlockZ() + 0.5));
+	        	plugin.saveConfig();
+	        	p.sendMessage(ChatColor.GREEN + "Deathmatch spawn for map 2 set!");
+        	}
+        }
+        } return true;
 	}
 }
